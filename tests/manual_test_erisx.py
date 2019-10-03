4829d063d88b6c7ad925e9dc5d603f38ce25aa96
Ed Noepel <ed@makerdao.com>
2019-10-02 23:55:27 -0400

    WebAPI call to retrieve balances is now functional

--- tests/manual_test_erisx.py
+++ tests/manual_test_erisx.py
@@ -5,7 +5,7 @@ from pyexchange.erisx import ErisxApi
 
 client = ErisxApi(fix_endpoint="127.0.0.1:1752", sender_comp_id="unknown", username="unknown", password="unknown",
                   clearing_url="https://clearing.newrelease.erisx.com/api/v1/",
-                  api_secret=sys.argv[1], api_key=sys.argv[2])
+                  api_key=sys.argv[1], api_secret=sys.argv[2])
 
 print(sys.argv)
 print("ErisxApi created\n")
