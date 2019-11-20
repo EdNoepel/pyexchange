b57933a9122f53e4de4d487c26c4e79858033ddd
Ed Noepel <ed@makerdao.com>
2019-11-20 09:22:39 -0500

    Checking in some pre-MCD work

--- tests/manual_test_erisx.py
+++ tests/manual_test_erisx.py
@@ -3,9 +3,11 @@ import sys
 from pyexchange.erisx import ErisxApi
 
 
-client = ErisxApi(fix_endpoint="127.0.0.1:1752", sender_comp_id="unknown", username="unknown", password="unknown",
+client = ErisxApi(fix_trading_endpoint=sys.argv[1], fix_trading_user=sys.argv[2],
+                  fix_marketdata_endpoint=sys.argv[3], fix_marketdata_user=sys.argv[4],
+                  password=sys.argv[5],
                   clearing_url="https://clearing.newrelease.erisx.com/api/v1/",
-                  api_key=sys.argv[1], api_secret=sys.argv[2])
+                  api_key=sys.argv[6], api_secret=sys.argv[7])
 
 print(sys.argv)
 print("ErisxApi created\n")
