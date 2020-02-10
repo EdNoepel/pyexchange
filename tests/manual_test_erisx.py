bf2368634ca1e4d72265306501810eade784d501
Ed Noepel <ed@makerdao.com>
2020-02-10 15:43:52 -0500

    Created a thread to manage the entire FIX session lifecycle, rather than trying to do so from within the PyexAPI thread.

--- tests/manual_test_erisx.py
+++ tests/manual_test_erisx.py
@@ -5,21 +5,21 @@ import time
 from pyexchange.erisx import ErisxApi
 
 
-logging.basicConfig(format='%(asctime)-15s %(levelname)-8s %(message)s', level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)-15s %(levelname)-8s %(threadName)-18s %(message)s', level=logging.DEBUG)
 client = ErisxApi(fix_trading_endpoint=sys.argv[1], fix_trading_user=sys.argv[2],
                   fix_marketdata_endpoint=sys.argv[3], fix_marketdata_user=sys.argv[4],
                   password=sys.argv[5],
                   clearing_url="https://clearing.newrelease.erisx.com/api/v1/",
                   api_key=sys.argv[6], api_secret=sys.argv[7])
-print(sys.argv)
+# print(sys.argv)
 print("ErisxApi created\n")
 
+# print(client.get_balances())
+time.sleep(15)
 
-def test_get_balances():
-    print(client.get_balances())
+print(f"Received security list with message length {len(str(client.get_markets()))}")
+time.sleep(25)
 
-
-time.sleep(5)
-test_get_balances()
+print("Disconnecting")
 del client
-time.sleep(3)
+time.sleep(2)
